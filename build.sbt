892651fa0f5fb787c8e843c18b71ee6f02724dfd
jvican <jorgevc@fastmail.es>
2018-02-08 14:42:21 +0100

    Set up release in sbt

--- build.sbt
+++ build.sbt
@@ -1,13 +1,6 @@
-inThisBuild(
-  List(
-    organization := "ch.epfl.scala",
-  )
-)
-
 val bsp = project
   .in(file("."))
   .settings(
-    resolvers += Resolver.bintrayRepo("scalameta", "maven"),
     libraryDependencies ++= List(
       "io.github.scalapb-json" %% "scalapb-circe" % "0.1.1",
       "org.scalameta" %% "lsp4s" % "4d51e1ae"
--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -1,11 +1,13 @@
 package build
 
-import sbt.{PluginTrigger, Plugins, Def, AutoPlugin, Keys}
+import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin
+import ch.epfl.scala.sbt.release.{AutoImported => ReleaseEarlyKeys}
+import sbt.{AutoPlugin, Compile, Def, Keys, PluginTrigger, Plugins}
+import sbtdynver.GitDescribeOutput
 
 object BuildPlugin extends AutoPlugin {
   import sbt.plugins.JvmPlugin
   import com.typesafe.sbt.SbtPgp
-  import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin
 
   override def trigger: PluginTrigger = allRequirements
   override def requires: Plugins = JvmPlugin && ReleaseEarlyPlugin && SbtPgp
@@ -27,9 +29,10 @@ object BuildKeys {
 object BuildImplementation {
   import sbtdynver.DynVerPlugin.{autoImport => DynVerKeys}
 
-  val projectSettings: Seq[Def.Setting[_]] = Nil
   val globalSettings: Seq[Def.Setting[_]] = Nil
   val buildSettings: Seq[Def.Setting[_]] = List(
+    Keys.organization := "ch.epfl.scala",
+    ReleaseEarlyKeys.releaseEarlyWith := ReleaseEarlyKeys.SonatypePublisher,
     BuildKeys.ourDynVerInstance :=
       sbtdynver.DynVer(Some(Keys.baseDirectory.in(sbt.ThisBuild).value)),
     DynVerKeys.dynver := BuildKeys.ourDynVerInstance.value.version(new java.util.Date),
@@ -38,4 +41,30 @@ object BuildImplementation {
       instance.getGitDescribeOutput(DynVerKeys.dynverCurrentDate.value)
     }
   )
+
+  val projectSettings: Seq[Def.Setting[_]] = List(
+    Keys.publishArtifact in (Compile, Keys.packageDoc) := {
+      val output = DynVerKeys.dynverGitDescribeOutput.value
+      val version = Keys.version.value
+      publishDocAndSourceArtifact(output, version)
+    },
+    Keys.publishArtifact in (Compile, Keys.packageSrc) := {
+      val output = DynVerKeys.dynverGitDescribeOutput.value
+      val version = Keys.version.value
+      publishDocAndSourceArtifact(output, version)
+    },
+  )
+
+  /**
+    * This setting figures out whether the version is a snapshot or not and configures
+    * the source and doc artifacts that are published by the build.
+    *
+    * Snapshot is a term with no clear definition. In this code, a snapshot is a revision
+    * that is dirty, e.g. has time metadata in its representation. In those cases, the
+    * build will not publish doc and source artifacts by any of the publishing actions.
+    */
+  def publishDocAndSourceArtifact(info: Option[GitDescribeOutput], version: String): Boolean = {
+    val isStable = info.map(_.dirtySuffix.value.isEmpty)
+    !isStable.map(stable => !stable || version.endsWith("-SNAPSHOT")).getOrElse(false)
+  }
 }
