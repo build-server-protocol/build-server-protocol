03e9b72d6a36d5a2b6e97f2cbcc85efa6d5291cd
jvican <jorgevc@fastmail.es>
2018-02-08 14:54:13 +0100

    Add metadata for publishing

--- project/BuildPlugin.scala
+++ project/BuildPlugin.scala
@@ -2,7 +2,7 @@ package build
 
 import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin
 import ch.epfl.scala.sbt.release.{AutoImported => ReleaseEarlyKeys}
-import sbt.{AutoPlugin, Compile, Def, Keys, PluginTrigger, Plugins}
+import sbt.{AutoPlugin, Compile, Def, Keys, PluginTrigger, Plugins, url, Developer}
 import sbtdynver.GitDescribeOutput
 
 object BuildPlugin extends AutoPlugin {
@@ -29,9 +29,28 @@ object BuildKeys {
 object BuildImplementation {
   import sbtdynver.DynVerPlugin.{autoImport => DynVerKeys}
 
-  val globalSettings: Seq[Def.Setting[_]] = Nil
+  // This should be added to upstream sbt.
+  private def GitHub(org: String, project: String): java.net.URL =
+    url(s"https://github.com/$org/$project")
+  private def GitHubDev(handle: String, fullName: String, email: String) =
+    Developer(handle, fullName, email, url(s"https://github.com/$handle"))
+
+  private final val ThisRepo = GitHub("scalacenter", "bloop")
+  val globalSettings: Seq[Def.Setting[_]] = List(
+    Keys.startYear := Some(2017),
+    Keys.autoAPIMappings := true,
+    Keys.publishMavenStyle := true,
+    Keys.homepage := Some(ThisRepo),
+    Keys.licenses := Seq("Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")),
+    Keys.developers := List(
+      GitHubDev("olafurpg", "Ólafur Páll Geirsson", "olafurpg@gmail.com"),
+      GitHubDev("jvican", "Jorge Vicente Cantero", "jorge@vican.me"),
+    ),
+  )
+
   val buildSettings: Seq[Def.Setting[_]] = List(
     Keys.organization := "ch.epfl.scala",
+    Keys.resolvers += sbt.Resolver.bintrayRepo("scalameta", "maven"),
     ReleaseEarlyKeys.releaseEarlyWith := ReleaseEarlyKeys.SonatypePublisher,
     BuildKeys.ourDynVerInstance :=
       sbtdynver.DynVer(Some(Keys.baseDirectory.in(sbt.ThisBuild).value)),
