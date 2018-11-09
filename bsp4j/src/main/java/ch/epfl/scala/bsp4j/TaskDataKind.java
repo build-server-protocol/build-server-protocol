1869046745eaf6e2a4e1707b9a2158d6af4e3450
Justin Kaeser <justin.kaeser@jetbrains.com>
2018-11-09 18:21:56 +0100

    String constants need to be final in Java

--- bsp4j/src/main/java/ch/epfl/scala/bsp4j/TaskDataKind.java
+++ bsp4j/src/main/java/ch/epfl/scala/bsp4j/TaskDataKind.java
@@ -1,9 +1,10 @@
 package ch.epfl.scala.bsp4j;
 
 public class TaskDataKind {
-    public static String COMPILE_TASK ="compile-task";
-    public static String TEST_TASK = "test-task";
-    public static String TEST_REPORT = "test-report";
-    public static String TEST_START = "test-start";
-    public static String TEST_FINISH = "test-finish";
+    public static final String COMPILE_TASK ="compile-task";
+    public static final String COMPILE_REPORT ="compile-report";
+    public static final String TEST_TASK = "test-task";
+    public static final String TEST_REPORT = "test-report";
+    public static final String TEST_START = "test-start";
+    public static final String TEST_FINISH = "test-finish";
 }
