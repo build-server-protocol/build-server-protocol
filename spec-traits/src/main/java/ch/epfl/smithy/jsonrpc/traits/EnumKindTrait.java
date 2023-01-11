2f634d632f91c922b3a9d2f430f43ee18282871e
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-01-11 13:43:47 +0100

    Added trait module and POJOs

---
+++ spec-traits/src/main/java/ch/epfl/smithy/jsonrpc/traits/EnumKindTrait.java
@@ -0,0 +1,85 @@
+package ch.epfl.smithy.jsonrpc.traits;
+
+import software.amazon.smithy.model.SourceException;
+import software.amazon.smithy.model.node.Node;
+import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.model.traits.AbstractTrait;
+import software.amazon.smithy.model.traits.AbstractTraitBuilder;
+import software.amazon.smithy.model.traits.TraitService;
+import software.amazon.smithy.utils.SmithyBuilder;
+import software.amazon.smithy.utils.ToSmithyBuilder;
+
+public class EnumKindTrait extends AbstractTrait implements ToSmithyBuilder<EnumKindTrait> {
+
+	public enum EnumKind {
+		OPEN, CLOSED
+	}
+
+	public static final ShapeId ID = ShapeId.from("jsonrpc#enumKind");
+
+	private final EnumKind enumKind;
+
+	private EnumKindTrait(Builder builder) {
+		super(ID, builder.getSourceLocation());
+		this.enumKind = builder.enumKind;
+		if (enumKind == null) {
+			throw new SourceException("An enumKind must be provided", getSourceLocation());
+		}
+	}
+
+	public EnumKind getEnumKind() {
+		return this.enumKind;
+	}
+
+	@Override
+	protected Node createNode() {
+		ObjectNode.Builder builder = Node.objectNodeBuilder();
+		// builder.withMember("mode", getEnumKind().name());
+		// builder.withMember("reportLevel", getCompatReportLevel().name());
+		return builder.build();
+	}
+
+	@Override
+	public SmithyBuilder<EnumKindTrait> toBuilder() {
+		return builder().enumKind(enumKind).sourceLocation(getSourceLocation());
+	}
+
+	/**
+	 * @return Returns a new EnumKindTrait builder.
+	 */
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public static final class Builder extends AbstractTraitBuilder<EnumKindTrait, Builder> {
+
+		private EnumKind enumKind;
+
+		public Builder enumKind(EnumKind enumKind) {
+			this.enumKind = enumKind;
+			return this;
+		}
+
+
+		@Override
+		public EnumKindTrait build() {
+			return new EnumKindTrait(this);
+		}
+	}
+
+	public static final class Provider implements TraitService {
+
+		@Override
+		public ShapeId getShapeId() {
+			return ID;
+		}
+
+		@Override
+		public EnumKindTrait createTrait(ShapeId target, Node value) {
+			EnumKind enumKind = EnumKind.valueOf(value.expectStringNode().getValue());
+			Builder builder = new Builder().enumKind(enumKind);
+			return new EnumKindTrait(builder);
+		}
+	}
+}
