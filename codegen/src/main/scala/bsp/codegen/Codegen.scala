2ffcbded7e3e45e356b99726f5445fd9b4487093
Olivier Mélois <olivier.melois@disneystreaming.com>
2023-01-12 16:20:57 +0100

    Drop un-necessary option

--- codegen/src/main/scala/bsp/codegen/Codegen.scala
+++ codegen/src/main/scala/bsp/codegen/Codegen.scala
@@ -8,7 +8,7 @@ object Codegen {
     val model = ModelLoader.loadModel()
     val definitions = new SmithyToIR(model).definitions("bsp")
     val renderer = new Renderer("ch.epfl.scala.bsp4j")
-    val codegenFiles = definitions.flatMap(renderer.render)
+    val codegenFiles = definitions.map(renderer.render)
     codegenFiles.map { cf =>
       val fullPath = outputDir / cf.path
       os.write.over(fullPath, cf.contents, createFolders = true)
