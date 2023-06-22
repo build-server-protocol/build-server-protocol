051101aa0f590669dabe1cb9a273c960d9d04405
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-06-22 14:26:44 +0200

    Refactor approval tests

---
+++ codegen/src/main/scala/bsp/codegen/ExtensionLoader.scala
@@ -0,0 +1,24 @@
+package bsp.codegen
+
+import java.nio.file.{Files, Paths}
+import scala.jdk.StreamConverters.StreamHasToScala
+
+object ExtensionLoader {
+  def loadExtensions(): List[String] = {
+    try {
+      val extensionsPath = Paths.get("spec/src/main/resources/META-INF/smithy/bsp/extensions")
+      val extensions = Files
+        .list(extensionsPath)
+        .toScala(List)
+        .map(_.getFileName.toString.split("\\.").head)
+      extensions
+    } catch {
+      case e: Throwable => throw new RuntimeException("Failed to load extensions, make sure that the working directory is set correctly", e)
+    }
+  }
+
+  def namespaces(): List[String] = {
+    val extensions = loadExtensions()
+    "bsp" :: extensions.map(ext => s"bsp.$ext")
+  }
+}
