02f62aa38321cd44122cb70885ef46fc5ede9eba
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-07-05 18:19:24 +0200

    Fix a flaky test (arguments were switched around)

--- testkit/src/main/scala/ch/epfl/scala/bsp/testkit/gen/Bsp4jGenerators.scala
+++ testkit/src/main/scala/ch/epfl/scala/bsp/testkit/gen/Bsp4jGenerators.scala
@@ -264,7 +264,7 @@ trait Bsp4jGenerators {
     rootUri <- genFileUriString
     capabilities <- genBuildClientCapabilities
   } yield {
-    val params = new InitializeBuildParams(displayName, version, bspVersion, rootUri, capabilities)
+    val params = new InitializeBuildParams(rootUri, displayName, version, bspVersion, capabilities)
     params.setData(null)
     params
   }
