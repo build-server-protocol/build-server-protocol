34233a637b029dba99d5fabe40e4be5115f1f151
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2023-11-24 15:40:12 +0100

    WIP

--- bsp4s/src/ch/epfl/scala/bsp/endpoints/Endpoints.scala
+++ bsp4s/src/ch/epfl/scala/bsp/endpoints/Endpoints.scala
@@ -291,6 +291,18 @@ trait BuildTarget {
   object rustWorkspace
       extends Endpoint[RustWorkspaceParams, RustWorkspaceResult]("buildTarget/rustWorkspace")
 
+  /** **Unstable** (may change in future versions) The Rust workspace request is sent from the
+    * client to the server to query for the information about project's workspace for the given list
+    * of build targets.
+    *
+    * The request is essential to connect and work with `intellij-rust` plugin.
+    *
+    * The request may take a long time, as it may require building a project to some extent (for
+    * example with `cargo check` command).
+    */
+  object rustWorkspace
+      extends Endpoint[RustWorkspaceParams, RustWorkspaceResult]("buildTarget/rustWorkspace")
+
   /** The build target scalac options request is sent from the client to the server to query for the
     * list of compiler options necessary to compile in a given list of targets.
     */
